ef3296a62098181ac6645d3f8ac0ea0cc1da8b74
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-07-14 11:39:25 -0300

    refactor: url path for swagger

--- api/config/swagger.py
+++ api/config/swagger.py
@@ -1,11 +1,11 @@
-from django.urls import path, path
+from django.urls import path, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="Pet Page API",
       default_version='v1',
       description="Test description",
       terms_of_service="https://www.google.com/policies/terms/",
@@ -17,7 +17,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-   path('swagger<format>', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-   path('swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-   path('redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+   re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
