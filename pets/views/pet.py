0b87d1cc108a77d411b12b7b477d441d96731f2d
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-07-25 16:09:16 -0300

    fix: bucket storage

--- pets/views/pet.py
+++ pets/views/pet.py
@@ -1,3 +1,4 @@
+import traceback
 import uuid
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -9,15 +10,14 @@ from pets.models.petInfo import Pet
 from pets.serializers.petInfoSerializer import PetSerializer
 from rest_framework.permissions import AllowAny
 from api.docs.doc import document_api
-from rest_framework.permissions import AllowAny
-from rest_framework.permissions import IsAuthenticated
+
 
 class PetView(APIView):
     def get_permissions(self):
         return get_permissions_by_method(
             self.request.method,
             get_perm=AllowAny,
-            post_perm=IsAuthenticated,
+            post_perm=AllowAny,
             put_perm=DenyAllPermission,
             patch_perm=DenyAllPermission,
             delete_perm=DenyAllPermission,
@@ -26,7 +26,7 @@ class PetView(APIView):
     def _upload_to_supabase(self, file):
         ALLOWED_EXTENSIONS = ["jpg", "jpeg", "png", "webp"]
         file_ext = file.name.split('.')[-1].lower()
-        
+
         if file_ext not in ALLOWED_EXTENSIONS:
             raise Exception(f"Extensão '{file_ext}' não permitida.")
 
@@ -40,36 +40,37 @@ class PetView(APIView):
 
         public_url = supabase.storage.from_("pets-avatar").get_public_url(path)
         return public_url
-    
+
     @document_api(PetSerializer, summary="Adicionar pet", request_body=True)
     def post(self, request):
         try:
-            foto = request.FILES.get('fotoUrl')
             data = request.data.copy()
             data['status'] = 'disponivel'
+
+            foto = request.FILES.get('fotoUrl')
 
             if foto:
                 foto_url = self._upload_to_supabase(foto)
                 data['fotoUrl'] = foto_url
             else:
-                if not data.get('fotoUrl'):
-                  return Response(
-                    {"fotoUrl": ["Este campo é obrigatório. Envie uma URL ou faça upload de uma imagem."]},
-                    status=status.HTTP_400_BAD_REQUEST
-                )
+                foto_url_str = data.get('fotoUrl')
+                if not foto_url_str:
+                    return Response(
+                        {"fotoUrl": ["Este campo é obrigatório. Envie uma URL ou faça upload de uma imagem."]},
+                        status=status.HTTP_400_BAD_REQUEST
+                    )
+                data['fotoUrl'] = foto_url_str
 
             serializer = PetSerializer(data=data)
-
             if serializer.is_valid():
                 serializer.save()
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
-
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
         except Exception as e:
+            print(traceback.format_exc())
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-    
-    
+
     @document_api(PetSerializer, Pet, summary="Listar pets")
     def get(self, request):
         return filtrar_e_listar(
@@ -77,6 +78,4 @@ class PetView(APIView):
             model=Pet,
             serializer_class=PetSerializer,
             not_found_message="Nenhum pet encontrado."
-    )
-
-
+        )
