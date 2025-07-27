8b743e9b836085094027ef9ad791f1dc05910e4c
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-07-27 10:05:02 -0300

    feat: secure swagger endpoint

--- api/config/swagger.py
+++ api/config/swagger.py
@@ -1,3 +1,4 @@
+import os
 from django.urls import path, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -17,8 +18,11 @@ schema_view = get_schema_view(
    authentication_classes=[],
 )
 
-urlpatterns = [
-   re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-]
+if os.environ.get('ENV') == 'development':
+    urlpatterns = [
+       re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+       path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+       path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    ]
+else:
+    urlpatterns = []
